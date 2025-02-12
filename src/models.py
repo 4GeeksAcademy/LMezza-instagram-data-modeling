526eb3e87486ee200b12e90abf76b13b1e4b90b4
LMezza <mezabalza18@gmail.com>
2025-02-12 11:38:56 +0000

    primera edicion

--- src/models.py
+++ src/models.py
@@ -1,26 +1,52 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
+from sqlalchemy import create_engine, ForeignKey, String, Column, Table
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+followers = Table(
+    "followers",
+    Base.metadata,
+    Column("user_from_id", ForeignKey("users.id")),
+    Column("users_to_id", ForeignKey("users.id")),
+)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(String(80), nullable=False, unique=True)
+    post: Mapped["Post"] = relationship(back_populates="users")
+    comment: Mapped["Comment"] = relationship(back_populates="users")
+
+class Post(Base):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="post")
+    comment: Mapped["Comment"] = relationship(back_populates="post")
+    media: Mapped["Media"] = relationship(back_populates="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="comment")
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped["Post"] = relationship(back_populates="comment")
+    
+class Media(Base):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped["Post"] = relationship(back_populates="media")
+
 
     def to_dict(self):
         return {}
